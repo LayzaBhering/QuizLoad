231e78c6fce3288eb42088f9ca9682a4085de062
Faell4328 <rhafahenrique@hotmail.com>
2025-02-24 15:17:40 -0300

    Adicionando estrutura da página iniciar quiz

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
-require_once("Public/home.html");
+$diretorio_atual = parse_url($_SERVER['REQUEST_URI'])["path"];
+$diretorio_atual = explode("/", $diretorio_atual);
+
+if($diretorio_atual[1] == ""){
+	require_once("Public/home.html");
+}
+else if($diretorio_atual[1] == "quiz"){
+	require_once("Public/quiz.html");
+}
+else{
+	echo "<p> Outra página não home</p>";
+}
 
 ?>
